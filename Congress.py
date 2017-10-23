65cca29a11e980888c1c83bad74cd2fe5aa151ed
nschue201 <nikolaus_schuetz@comcast.com>
2017-10-23 17:04:32 -0400

    Interim commit for at-home editing

--- Congress.py
+++ Congress.py
@@ -6,17 +6,188 @@ Created on Sun Oct 15 17:31:47 2017
 @author: nschue201
 """
 
+#import os
+#import json
+
+#import numpy as np
+
+from utils import Utils, GeojsonParser
+
 class Congress(object):
     
-    """
-    session_number : int, the session of this congress
-    """
-    
-    distrits
-    session = None
-    
-    
     DATA_PATH = "./resources/data/"
+    is_print = True
+    # fill this with districts
+    districts = []
+    sessions = {}
     
-    def __init__(self):
+    gjp = GeojsonParser()
+    
+    def __init__(self, ncong=114, is_print=True):
+        self.ncong = ncong
+        self.is_print = is_print
+    
+    def fit(self):
+        # make sessions
+        self.make_sessions()
+        # make the districts
+        self.make_districts(self.is_print)
+        # build sessions: sort the districts into state-session
+        self.build_sessions()
+    
+    def make_districts(self, is_print=True):
+        # work through the files
+        for kwargs in self.gjp.parse_features(is_print):
+            cong = kwargs.get("cong")
+            feature = kwargs.get("feature")
+            d = District(feature)
+            d.fit(cong)
+            self.districts.append(d)
+    
+    def build_sessions(self):
+        for d in self.districts:
+            self.add_district(d)
+    
+    def make_sessions(self):
+        for cong in Utils.str_range("1", self.ncong):
+            self.sessions[cong] = Session(cong)
+
+    def add_district(self, d):
+        # pass the district to the corret session
+        self.sessions[d.cong].add_district(d)
+        
+    def add_session(self, cong):
+        self.sessions[cong] = Session(cong)
+    
+    def has_session(self, cong):
+        return cong in self.sessions.keys()            
+    
+
+class Session(object):
+    
+    states = {}
+    
+    def __init__(self, cong):
+        self.cong = cong
+    
+    def has_state(self, statecd):
+        return statecd in self.states.keys()
+    
+    def add_state(self, d):
+        self.states[d.get_statecd()] = State(d)
+    
+    def add_district(self, d):
+        # add the state if it needs it
+        statecd = d.get_statecd()
+        if self.has_state(statecd):
+            self.states[statecd].add_district(d)
+        else:
+            self.add_state(d)
+            self.states[statecd].add_district(d)
+    
+    def get_state(self, statecd):
+        return self.states.get(statecd)
+
+class State(object):
+    
+    districts = []
+    
+    def __init__(self, d):
+        self.cong = d.cong
+        self.statecd = d.get_statecd()
+        self.feature = d.feature
+        
+    def fit(self, cong=None):
         pass
+    
+    def add_district(self, d):
+        self.districts.append(d)
+    
+    def get_district(self, district):
+        for d in self.districts:
+            if d.get_district() == district:
+                return d
+    
+    def has_district(self, d):
+        for district in self.districts:
+            if d.get_district_id() == district.get_district_id():
+                return True
+        return False
+
+
+class District(object):
+        
+    def __init__(self, feature, cong=None):
+        self.feature = feature
+        self.geometry = self.feature.get("geometry")
+        self.properties = self.feature.get("properties")
+        self.coordinates = self.geometry.get('coordinates')
+        if cong is not None:
+            self.fit(cong)
+    
+    def fit(self, cong=None):
+        if cong:
+            self.cong = cong
+        self.district_properties = DistrictProperties(self.feature.get("properties"))
+        self.district_properties.fit(self.cong)
+    
+    def get_cong(self):
+        return self.cong
+    
+    def get_keys(self):
+        return {"cong":self.cong,
+                "statecd":self.district_properties.statecd}
+        self.properties
+    
+    def get_statecd(self):
+        return self.district_properties.statecd
+    
+    def get_district(self, district):
+        return self.district_properties.get_district()
+    
+    def get_district_id(self):
+        return self.district_properties.district_id
+    
+class DistrictProperties(object):
+    
+    def __init__(self, properties):
+        self.properties = properties
+        # get data from properties
+        self.district = self.properties.get("district")
+        self.endcong = self.properties.get("endcong")
+        self.district_id = self.properties.get("id") # id is python keyword
+        self.startcong = self.properties.get("startcong")
+        self.statename = self.properties.get("statename")
+        self.statecd = Utils.get_statecd(self.statename)
+        self.members = self.properties.get("member")
+        
+    def fit(self, cong):
+        self.cong = cong
+        self.member = Member(self.members)
+        self.member.fit(self.cong)
+    
+    def get_district(self):
+        return self.district
+
+class Member(object):
+    
+    """name : string, name of congress member
+    party : string, party of congress member
+    """
+    
+    data = {}
+    member_id = None
+    name = None
+    party = None
+    
+    def __init__(self, members):
+        self.members = members
+        
+    def fit(self, cong):
+        self.cong = cong
+        member = self.members.get(self.cong)
+        if isinstance(member, (dict)):
+            self.member_id = list(member.keys())[0]
+            self.data = member.get(self.member_id)
+            self.name = self.data.get("name")
+            self.party = self.data.get("party")
--- Utils.py
+++ Utils.py
@@ -1,17 +1,63 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sun Oct 15 17:46:24 2017
+Created on Mon Oct 16 07:27:18 2017
 
 @author: nschue201
 """
 
+import json
+import os
+
 class Utils(object):
     
+    state_codes_path = "./resources/state_codes.json"
+    with open(state_codes_path, "r") as f:
+        states = json.load(f)
+        state_names = dict((x,y) for x,y in states.get("states"))
+        state_codes = dict((y,x) for x,y in states.get("states"))
+    
+    def str_range(start, stop):
+        """helper function to make a range of str(int)"""
+        for x in range(int(start), int(stop) + 1):
+            yield str(x)
+            
+    def get_statecd(statename):
+        return Utils.state_names.get(statename)
+    
+    def get_statename(statecd):
+        return Utils.state_codes.get(statecd)
+
+
+
+class GeojsonParser(object):
+    
+    data_path = "./resources/data"
+    geojson_path = "JeffreyBLewis/congressional-district-boundaries"
+    
     def __init__(self):
-        pass
+        self.path = "%s/%s" % (self.data_path, self.geojson_path)
+        self.geojson_files = [x for x in os.listdir(self.path)
+                              if x.split(".")[-1] == "geojson"]
     
-    def parse_member(self, gejson):
-        pass
+    def get_geojson(self, filename):
+        with open("%s/%s" % (self.path, filename), "r") as f:
+            return json.load(f)
     
-    def parse_term(self, geojson):
+    def get_features(self, geojson):
+        return geojson.get('features')
+    
+    def parse_features(self, is_print=True):
+        
+        for filename in self.geojson_files:
+            if is_print:
+                print("parsing %s" % filename)
+            with open("%s/%s" % (self.path, filename), "r") as f:
+                geojson = json.load(f)
+            features = geojson.get("features")
+            for feature in features:
+                properties = feature.get("properties")
+                startcong = properties.get("startcong")
+                endcong = properties.get("endcong")
+                for cong in Utils.str_range(startcong, endcong):
+                    yield {"feature":feature,"cong":cong}
